11de58aa3b01e2a2ef4c1b6cd5d8bda982bcfd52
IvashkaVIP <ivashka.co@gmail.com>
2023-09-09 22:23:16 +0300

    add temlate

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contacts = require("./models/contacts.json");
+const fs = require("fs/promises");
+const moment = require("moment");
 
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  const { url, method } = req;
+  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  await fs.appendFile("./server.log", `\n${date} ${method} ${url}`);
+  next();
+});
+
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+app.get("/api/contacts/:id", (req, res) => {
+  res.json(contacts[0]);
+});
+
+app.post("/api/contacts", (req, res) => {
+  res.json(contacts[1]);
+});
+
+app.delete("/api/contacts/:id", (req, res) => {
+  res.json(contacts[2]);
+});
+
+app.put("/api/contacts/:id", (req, res) => {
+  res.json(contacts[3]);
+});
+
+// @ POST /api/contacts
+// @ DELETE /api/contacts/:id
+// @ PUT /api/contacts/:id
+
+// app.get("/api/contacts", (req, res) => {
+//   console.log(req.method);
+//   console.log(req.url);
+//   res.json(contacts);
+//   // res.send(contacts);
+// });
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
