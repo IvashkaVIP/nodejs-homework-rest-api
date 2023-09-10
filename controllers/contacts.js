7ed6d39976964ebd0475219e3d739745f771ca3b
IvashkaVIP <ivashka.co@gmail.com>
2023-09-10 17:30:48 +0300

    add ctrlWrapper

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,7 @@
 const contacts = require("../models/contacts");
 const HttpError = require("../helpers/HttpErrors");
 const Joi = require("joi");
+const ctrlWrapper = require("../helpers/ctrlWrapper");
 
 const addShema = Joi.object({
   name: Joi.string().required(),
@@ -8,64 +9,44 @@ const addShema = Joi.object({
   phone: Joi.string().required(),
 });
 
-const listContacts = async (req, res, next) => {
-  try {
+const listContacts = async (req, res) => {
     res.status(200).json(await contacts.listContacts());
-  } catch (err) {
-    next(err);
-  }
 };
 
-const getContactById = async (req, res, next) => {
-  try {
+const getContactById = async (req, res) => {
     const { id } = req.params;
     const result = await contacts.getContactById(id);
     if (!result) throw HttpError(404, "Not found");
-    res.status(200).json(result);
-  } catch (err) {
-    next(err);
-  }
+    res.status(200).json(result);  
 };
 
 const addContact = async (req, res, next) => {
-  try {
     const { error } = addShema.validate(req.body);
     if (error) throw HttpError(400, error.message);
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
-  } catch (err) {
-    next(err);
-  }
 };
 
 const removeContact = async (req, res, next) => {
-  try {
     const { id } = req.params;
     const result = await contacts.removeContact(id);
     if (!result) throw HttpError(404, "Not found");
     res.status(200).json({ message: "contact deleted" });
-  } catch (err) {
-    next(err);
-  }
 };
 
 const updateContact = async (req, res, next) => {
-  try {
     const { error } = addShema.validate(req.body);
     if (error) throw HttpError(400, error.message);
     const { id } = req.params;
     const result = await contacts.updateContact(id, req.body);
     if (!result) throw HttpError(404, "Not found");
     res.status(201).json(result);
-  } catch (err) {
-    next(err);
-  }
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
 };
