24fbcead4bcc3dad0e01d2b378e4bb6da82b7f2a
IvashkaVIP <ivashka.co@gmail.com>
2023-09-11 17:40:05 +0300

    fix remarks

--- shemas/contacts.js
+++ shemas/contacts.js
@@ -1,9 +1,21 @@
 const Joi = require("joi");
 
 const addShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string()
+    .min(2)
+    .max(40)
+    .message("Name must be a minimum of 2 and a maximum of 40 characters")
+    .required(),
+  email: Joi.string()
+    .email()
+    .pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .message("Email should have the format example@email.com")
+    .required(),
+  phone: Joi.string()
+    .min(2)
+    .max(40)
+    .message("Phone must be a minimum of 2 and a maximum of 40 characters")
+    .required(),
 });
 
 module.exports = {
