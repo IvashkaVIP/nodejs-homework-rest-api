e31936ded74ff51a03fc826e3e277daf1fb76004
IvashkaVIP <ivashka.co@gmail.com>
2023-09-16 23:51:12 +0300

    add mongoose

--- app.js
+++ app.js
@@ -21,3 +21,4 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Volodymyr:<password>@cluster0.1tdggh2.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
+mongoose
+  .connect(BD_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((err) => console.log(err.message));
 
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
