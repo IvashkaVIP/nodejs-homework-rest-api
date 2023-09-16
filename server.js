f17cb3b922a031edb0b3a05db03185137bd043c2
IvashkaVIP <ivashka.co@gmail.com>
2023-09-17 00:53:33 +0300

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ mongoose
     app.listen(3000);
     console.log("Database connection successful");
   })
-  .catch((err) => console.log(err.message));
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
