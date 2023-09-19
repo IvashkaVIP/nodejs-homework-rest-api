9c5a8d16881a531b3b0525eff8e3ce31df180e99
IvashkaVIP <ivashka.co@gmail.com>
2023-09-19 22:35:09 +0300

    modify handleError, add bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,15 @@
 const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
 const ctrlWrapper = require("../helpers/ctrlWrapper");
-const { HttpError } = require("../helpers/HttpErrors");
+const HttpError = require("../helpers/HttpErrors");
 
 const register = async (req, res) => {
-    const newUser = await User.create(req.body);
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) throw HttpError(409, "Email already in use");
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({...req.body, password: hashPassword});
     res.status(201).json({ email: newUser.email, password: newUser.password });
 }
 
