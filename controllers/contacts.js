11e34d3440da33df6af72c9e5990b30f0701bfee
IvashkaVIP <ivashka.co@gmail.com>
2023-09-23 22:56:54 +0300

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,10 @@ const { Contact } = require("../models/contact");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({owner});
+  const { page = 1, limit = 10 } = req.query;
+  // console.log(page, "   ",limit);
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({owner}).skip(skip).limit(limit);
   res.status(200).json(result);
 };
 
