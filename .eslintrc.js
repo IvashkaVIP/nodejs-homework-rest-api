c1839dc5f17edd96616c81539287d0ba893d4785
IvashkaVIP <ivashka.co@gmail.com>
2023-10-01 21:32:42 +0300

    add plugin for eslint from jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
+  plugins: ["jest"],
+
   env: {
     commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ["standard", "prettier", "plugin:jest/recommended"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {},
-}
+};
